6884e1c71c633a4b5b5925812348ad94ed7f1d93
liuqinghua96 <865708365@qq.com>
2019-07-12 15:25:35 +0800

    主页布局-调用轮播图组件

--- src/module/first/index.js
+++ src/module/first/index.js
@@ -1,9 +1,37 @@
 import React from 'react'
+import ImageGallery from 'react-image-gallery'
+import "react-image-gallery/styles/css/image-gallery.css"
+import {Input} from 'semantic-ui-react'
 import './index.css'
 
 class First extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      calleryList: [{
+        original: 'http://47.96.21.88:8086/public/1.png'
+      }, {
+        original: 'http://47.96.21.88:8086/public/2.png'
+      }, {
+        original: 'http://47.96.21.88:8086/public/3.png'
+      }]
+    }
+  }
   render () {
-    return (<div>首页</div>)
+    return (<div className="home-container">
+      <div className="home-topbar">
+        <Input fluid icon='search' placeholder='请输入关键字...'/>
+      </div>
+      <div className="home-content">
+        <ImageGallery 
+          showThumbnails={false}
+          showNav={false}
+          autoPlay={true}
+          showFullscreenButton={false}
+          showPlayButton={false}
+          items={this.state.calleryList} />
+      </div>
+    </div>)
   }
 }
 
