3b5f3bfbed6a25ff9334e54d7fe3b71333b663d0
liuqinghua96 <865708365@qq.com>
2019-07-13 19:25:26 +0800

    获取房源列表数据&渲染页面

--- src/module/first/list.js
+++ src/module/first/list.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import './index.css'
 import http from '../../http'
 import {withRouter} from 'react-router-dom'
 import {Icon, Item} from 'semantic-ui-react'
@@ -17,6 +16,23 @@ class List extends React.Component {
     this.props.history.goBack()
   }
   render() {
+    let listInfo = this.state.list.map(item=>{
+      return (
+        <Item key={item.id}>
+          <Item.Image src={'http://47.96.21.88:8086/public/home.png'}/>
+          <Item.Content>
+            <Item.Header>{item.home_name}</Item.Header>
+            <Item.Meta>
+              <span className='cinema'>{item.home_desc}</span>
+            </Item.Meta>
+            <Item.Description>
+              {item.home_tags}
+            </Item.Description>
+            <Item.Description>{item.home_price}</Item.Description>
+          </Item.Content>
+        </Item>
+      );
+    });
     return (<div className="house-list">
       <div className="house-list-title">
         <Icon onClick={this.goBack} name='angle left' size='large'/>
@@ -25,7 +41,7 @@ class List extends React.Component {
       <div className="house-list-content">
         {/*房源列表*/}
         <Item.Group divided unstackable>
-          12313243
+          {listInfo}
         </Item.Group>
       </div>
     </div>)
@@ -34,8 +50,8 @@ class List extends React.Component {
     this.setState({
       title: this.props.location.state.name,
       type: this.props.location.state.type
-    },()=>{
-      let {data} = http.post('/homes/list',{home_type: this.state.type})
+    },async ()=>{
+      let {data} = await http.post('/homes/list',{home_type: this.state.type})
       this.setState({
         list: data
       })
