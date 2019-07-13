a99013dd395348bf69f29fd0ebfdfd2a75881a65
liuqinghua96 <865708365@qq.com>
2019-07-13 19:50:20 +0800

    计算器选项卡切换

--- src/module/first/calc.js
+++ src/module/first/calc.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import {withRouter} from 'react-router-dom'
+import {Icon,Tab} from 'semantic-ui-react'
 
-class Calc extends React.Component {
+// 公积金贷款组件
+class Loan extends React.Component {
   render () {
-    return (<div>计算器</div>)
+    return (<div>公积金贷款组件</div>)
   }
 }
 
-export default Calc
+
+class Calc extends React.Component {
+  goBack= () => {
+    this.props.history.goBack()
+  }
+  render () {
+    let PanesData = [
+      {menuItem:'公积金贷款', render: () => <Tab.Pane><Loan/></Tab.Pane>},
+      {menuItem:'商业贷款', render: () => <Tab.Pane>商业贷款</Tab.Pane>},
+      {menuItem:'组合贷款', render: () => <Tab.Pane>组合贷款</Tab.Pane>}
+    ];
+    return (<div className="home-calc">
+      <div className="home-calc-title">
+        <Icon onClick={this.goBack} name = 'angle left' size = 'large'/>贷款利率计算 
+      </div>
+      <div className="map-calc-content">
+        {/*选项卡布局*/}
+        <Tab  panes={PanesData}/>
+      </div>
+    </div>)
+  }
+}
+
+export default withRouter(Calc)
