981b2f0ff605a0c4b436db9001f00fdde702f7d8
liuqinghua96 <865708365@qq.com>
2019-07-15 20:20:32 +0800

    私聊页面数据展示

--- src/module/chat/chatWindow.js
+++ src/module/chat/chatWindow.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './chatWindow.css'
 import {withRouter} from 'react-router-dom'
+import {Form, Icon, Button, TextArea} from 'semantic-ui-react'
+import http from '../../http'
 
 class ChatWindow extends React.Component {
   constructor(props) {
@@ -8,18 +10,54 @@ class ChatWindow extends React.Component {
     this.state = {
       uname: '',
       fromUser: '',
-      toUser: ''
+      toUser: '',
+      list: []
     }
   }
-  render () {
-    return (<div>{this.state.uname}</div>)
+  goBack = () => {
+    this.props.history.goBack()
   }
-  componentDidMount () {
+  render() {
+    let listInfo = this.state.list.map(item=>{
+      return (
+        <li key={item.id}>
+          <img src={'http://47.96.21.88:8086/' + item.avatar} alt=""/>
+          <span>{item.chat_msg}</span>
+        </li>
+      );
+    })
+    return (
+      <div className="chat-window">
+        <div className="chat-window-title">
+           <Icon onClick={this.goBack} name='angle left' className='chat-ret-btn' size='large'/>
+           <span>{this.state.uname}</span>
+        </div>
+        <div className="chat-window-content">
+          <ul>
+            {listInfo}
+          </ul>
+        </div>
+        <div className="chat-window-input">
+          <Form>
+            <TextArea placeholder='请输入内容...' />
+            <Button >关闭</Button>
+            <Button primary >发送</Button>
+          </Form>
+        </div>
+      </div>
+    );
+  }
+  async componentDidMount () {
     let {uname,fromUser,toUser} = this.props.location.state
+    let {data: {list}} = await http.post('chats/info',{
+      from_user:fromUser,
+      to_user: toUser
+    })
     this.setState({
       uname,
       fromUser,
-      toUser
+      toUser,
+      list
     })
   }
 }
